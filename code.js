3009a083f970994d6500f7ab1963be9dc71b8506
Zachary Zhang <zzhang571@student.fuhsd.org>
2023-07-13 11:34:05 -0700

    Pasted from Personal File

---
+++ code.js
@@ -0,0 +1,6 @@
+function changeColor() {
+    //var name = document.getElementById('name').value;
+   // var greeting = 'Hello, ' + name + '!';
+    document.body.style.backgroundColor = "rgb(0, 0, 255)";
+
+}
